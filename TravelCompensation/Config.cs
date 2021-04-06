26b5bbe9218d0bd9bb2362b922dd1c5ad5260520
Synne Fog <synne.fog@gmail.com>
2021-04-06 21:01:36 +0200

    Add config values

---
+++ TravelCompensation/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelCompensation
+{
+    public class Config
+    {
+        public int MAX_DISTANCE_IN_KM = 75000;
+        public int LOWER_DISTANCE_LIMIT_IN_KM = 50000;
+        public double LOWER_DISTANCE_COMPENSATION = 1.5;
+        public double HIGHER_DISTANCE_COMPENSATION = 0.7;
+
+        public int LOWER_TOLL_LIMIT = 34000;
+
+        public int EXCESS = 22000;
+    }
+}
