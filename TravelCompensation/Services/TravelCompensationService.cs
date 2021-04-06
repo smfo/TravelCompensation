0372aca7615d14f63fab05b45002141b819ca340
Synne Fog <synne.fog@gmail.com>
2021-04-06 21:39:27 +0200

    Add service

---
+++ TravelCompensation/Services/TravelCompensationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelCompensation.Models;
+using TravelCompensation.Services.Interfaces;
+
+namespace TravelCompensation.Services
+{
+    class TravelCompensationService : ITravelCompensationService
+    {
+        public double CalculateCompensation(Compensation compensation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
