85942fed80f5c134e0d14abd2b0c9747dc690351
Synne Fog <synne.fog@gmail.com>
2021-04-06 21:39:17 +0200

    Add interface

---
+++ TravelCompensation/Services/Interfaces/ITravelCompensationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelCompensation.Models;
+
+namespace TravelCompensation.Services.Interfaces
+{
+    interface ITravelCompensationService
+    {
+        public double CalculateCompensation(Compensation compensation);
+    }
+}
