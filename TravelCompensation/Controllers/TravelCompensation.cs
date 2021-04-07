19d710f71531751d695c6cb781da60d99a48810d
Synne Fog <synne.fog@gmail.com>
2021-04-07 18:48:46 +0200

    Add working service

--- TravelCompensation/Controllers/TravelCompensation.cs
+++ TravelCompensation/Controllers/TravelCompensation.cs
@@ -8,6 +8,7 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using TravelCompensation.Services.Interfaces;
 using TravelCompensation.Services;
+using TravelCompensation.Models;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -24,14 +25,13 @@ namespace TravelCompensation
             CompensationService = new TravelCompensationService();
         }
 
-        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        public APIGatewayProxyResponse Get(APIGatewayProxyRequest requeste)
         {
-            context.Logger.LogLine("Get Request\n");
 
             var response = new APIGatewayProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
-                Body = "Travel compensation",
+                Body = CompensationService.CalculateCompensation(new Compensation()).ToString(),
                 Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
             };
 
--- TravelCompensation/Services/TravelCompensationService.cs
+++ TravelCompensation/Services/TravelCompensationService.cs
@@ -8,9 +8,70 @@ namespace TravelCompensation.Services
 {
     public class TravelCompensationService : ITravelCompensationService
     {
+
+        private Config config = new Config();
+
         public double CalculateCompensation(Compensation compensation)
         {
-            return 50000;
+            double workTripsCost = TravelCosts(compensation.WorkTrips);
+            double visitTravelsCost = TravelCosts(compensation.VisitingTravels);
+
+            double tollsCosts = TollCost(compensation.ToolExpenses);
+
+            double totalTravelCosts = workTripsCost + visitTravelsCost + tollsCosts;
+
+            double travelCompensation = totalTravelCosts >= config.EXCESS ? totalTravelCosts - config.EXCESS : 0;
+
+            return travelCompensation;
+        }
+
+        private double TollCost(double tollExpenses)
+        {
+            return tollExpenses >= config.LOWER_TOLL_LIMIT ? tollExpenses : 0;
+        }
+
+        private double TravelCosts(Travel[] travels)
+        {
+            double totalTravelCosts = 0;
+
+            foreach(Travel travel in travels)
+            {
+                double costPerTrip;
+                double[] distances = CalculateDistancesToCompensate(travel);
+
+                costPerTrip = distances[0] * config.LOWER_DISTANCE_COMPENSATION + distances[1] * config.HIGHER_DISTANCE_COMPENSATION;
+
+                totalTravelCosts += costPerTrip;
+            }
+
+            return totalTravelCosts;
+        }
+
+        private double[] CalculateDistancesToCompensate(Travel travel)
+        {
+            double lowCompensationDistance;
+            double highCompensationDistance;
+
+            double distance = travel.Distance;
+            if (distance > config.MAX_DISTANCE_IN_KM)
+            {
+                distance = config.MAX_DISTANCE_IN_KM;
+            }
+
+            if (distance <= config.LOWER_DISTANCE_LIMIT_IN_KM)
+            {
+                lowCompensationDistance = distance;
+                highCompensationDistance = distance - lowCompensationDistance;
+            }
+            else
+            {
+                lowCompensationDistance = config.LOWER_DISTANCE_LIMIT_IN_KM;
+                highCompensationDistance = 0;
+            }
+
+            double[] distances = new double[] { lowCompensationDistance, highCompensationDistance };
+
+            return distances;
         }
     }
 }
